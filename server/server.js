5842982ad7962ef0352aac411ea7b8c091a9e8e1
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-08 08:42:45 +0500

    Add server.js file with basic setup and routing

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require("./router/route.js");
+
+const connect = require("./database/");
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  try {
+    res.json("Get Request");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("Invalid Database Connection");
+  });
