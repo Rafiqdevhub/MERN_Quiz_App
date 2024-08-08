54a72addb2ab6778ed31bafceb1c0650a8c2efe6
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-08 08:43:27 +0500

    Add data.js file with quiz questions and answers

---
+++ server/data/data.js
@@ -0,0 +1,127 @@
+export default [
+  {
+    id: 1,
+    question: "Javascript is an _______ language",
+    options: ["Object-Oriented", "Object-Based", "Procedural"],
+  },
+  {
+    id: 2,
+    question:
+      "Following methods can be used to display data in some form using Javascript",
+    options: ["document.write()", "console.log()", "window.alert()"],
+  },
+  {
+    id: 3,
+    question:
+      "When an operator value is NULL, the typeof returned by the unary operator is:",
+    options: ["Boolean", "Undefined", "Object"],
+  },
+  {
+    id: 4,
+    question: "What does the toString() method return?",
+    options: ["Return Object", "Return String", "Return Integer"],
+  },
+  {
+    id: 5,
+    question:
+      "Which function is used to serialize an object into a JSON string?",
+    options: ["stringify()", "parse()", "convert()"],
+  },
+  {
+    id: 6,
+    question: "Which company developed JavaScript?",
+    options: ["Netscape", "Microsoft", "Sun Microsystems"],
+  },
+  {
+    id: 7,
+    question: "Which type of JavaScript language is ___",
+    options: ["Object-Oriented", "Object-Based", "Assembly-language"],
+  },
+  {
+    id: 8,
+    question: "In which HTML element do we put the JavaScript?",
+    options: ["<script>", "<javascript>", "<js>"],
+  },
+  {
+    id: 9,
+    question: "The correct syntax to output 'Hello World' in JavaScript is ___",
+    options: [
+      'console.log("Hello World")',
+      'document.write("Hello World")',
+      'print("Hello World")',
+    ],
+  },
+  {
+    id: 10,
+    question: "Which of the following is a JavaScript framework?",
+    options: ["Angular", "Laravel", "Django"],
+  },
+  {
+    id: 11,
+    question: "JavaScript code can be called by using ___",
+    options: ["Function/Method", "RMI", "Triggering Event"],
+  },
+  {
+    id: 12,
+    question: "Which symbol is used for comments in JavaScript?",
+    options: ["//", "/* */", "#"],
+  },
+  {
+    id: 13,
+    question: "What is the correct JavaScript syntax to write 'Hello World'?",
+    options: [
+      'document.write("Hello World")',
+      'response.write("Hello World")',
+      'echo("Hello World")',
+    ],
+  },
+  {
+    id: 14,
+    question: "Which of the following is not a reserved word in JavaScript?",
+    options: ["interface", "throws", "program"],
+  },
+  {
+    id: 15,
+    question:
+      "What is the correct place to insert JavaScript in an HTML document?",
+    options: ["Both <head> and <body>", "<head>", "<body>"],
+  },
+  {
+    id: 16,
+    question: "Which of the following is correct about features of JavaScript?",
+    options: [
+      "It can handle dates and time.",
+      "JavaScript is a object-based scripting language.",
+      "Both of the above.",
+    ],
+  },
+  {
+    id: 17,
+    question: "Which of the following is not a valid JavaScript variable name?",
+    options: ["2names", "_first_and_last_names", "FirstAndLast"],
+  },
+  {
+    id: 18,
+    question: "Why JavaScript is called a lightweight programming language?",
+    options: [
+      "Because it can provide programming functionality inside but up to certain extend.",
+      "Because it is used for client-side scripting.",
+      "Both of the above.",
+    ],
+  },
+  {
+    id: 19,
+    question: "Which built-in method sorts the elements of an array?",
+    options: ["sort()", "changeOrder(order)", "order()"],
+  },
+  {
+    id: 20,
+    question:
+      "Which of the following function of Number object forces a number to display in exponential notation?",
+    options: ["toExponential()", "toFixed()", "toPrecision()"],
+  },
+];
+
+export const answers = [
+  0, 1, 2, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0,
+];
