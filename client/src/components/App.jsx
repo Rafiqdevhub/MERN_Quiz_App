3356b6af0f2239dec61052a59f5cfde97b1b42ef
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-09 16:42:30 +0500

    Add Quiz and Result components for managing quiz results and questions

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,31 @@
 import "../styles/App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./Main";
+import Quiz from "./Quiz";
+import Result from "./Result";
+import { CheckUserExist } from "../helper/helper";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+    },
+    {
+      path: "/quiz",
+      element: (
+        // <CheckUserExist>
+        <Quiz />
+        // </CheckUserExist>
+      ),
+    },
+    {
+      path: "/result",
+      element: (
+        <CheckUserExist>
+          <Result />
+        </CheckUserExist>
+      ),
     },
   ]);
   return (
