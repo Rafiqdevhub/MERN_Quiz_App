de1493b362a98592f5acd5ce740118e856874080
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-13 07:27:45 +0500

    Refactor Main component to update quiz instructions and points awarded

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -16,11 +16,11 @@ export default function Main() {
 
   return (
     <div className="container">
-      <h1 className="title text-light">Quiz Application</h1>
+      <h1 className="title text-light">Quiz App</h1>
 
       <ol>
-        <li>You will be asked 10 questions one after another.</li>
-        <li>10 points is awarded for the correct answer.</li>
+        <li>You will be asked 20 questions one after another.</li>
+        <li>20 points is awarded for the correct answer.</li>
         <li>
           Each question has three options. You can choose only one options.
         </li>
